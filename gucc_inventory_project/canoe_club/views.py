9569618a414e5e216fbac4abee58ce1625f7fb33
Kai Hely <2661378h@student.gla.ac.uk>
2023-02-28 16:16:38 +0000

    added bootstrap to base template

--- gucc_inventory_project/canoe_club/views.py
+++ gucc_inventory_project/canoe_club/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("this is the index pag")
+    # return HttpResponse("this is the index pag")
+    return render(request, 'canoe_club/index.html')
 # Create your views here.
