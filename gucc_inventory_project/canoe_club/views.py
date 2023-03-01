2c1324a27d96a4298683b52606d54aac3344ca37
Renee Wallis <2560885w@student.gla.ac.uk>
2023-03-01 14:11:31 +0000

    changed display of upcoming events

--- gucc_inventory_project/canoe_club/views.py
+++ gucc_inventory_project/canoe_club/views.py
@@ -3,17 +3,12 @@ from canoe_club.models import Trip, Social
 import datetime
 
 def index(request):
-    handleTime(Trip)
-    handleTime(Social)
-    trips_list = Trip.objects.order_by("-date")[:5]
-    social_list = Social.objects.order_by("-date")[:5]
+    today = datetime.datetime.today()
+    trips_list = Trip.objects.filter(date__gte=today).order_by("-date")[:5]
+    social_list = Social.objects.filter(date__gte=today).order_by("-date")[:5]
 
     context_dixt = {}
     context_dixt["trips"] = trips_list
     context_dixt["socials"] = social_list
     return render(request, 'canoe_club/index.html', context_dixt)
 # Create your views here.
-
-def handleTime(model):
-    model.objects.filter(date=datetime.datetime.now() - datetime.timedelta(days=1)).delete()
-    return
