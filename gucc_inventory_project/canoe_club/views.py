5f99a9a2b78c623eba1bdb96c534ab3cab8ec54d
Ayleen Sohaib <2684413S@student.gla.ac.uk>
2023-03-03 15:56:52 +0000

    added views

--- gucc_inventory_project/canoe_club/views.py
+++ gucc_inventory_project/canoe_club/views.py
@@ -22,36 +22,21 @@ def about(request):
 def main_shed(request):
     return render(request, 'canoe_club/main_shed.html')
 
+def add_kit(request):
+    return render(request, 'canoe_club/main_shed/kit/add_kit.html')
+
+def remove_kit(request):
+    return render(request, 'canoe_club/main_shed/kit/remove_kit.html')
+
+def move_shed(request):
+    return render(request, 'canoe_club/main_shed/kit/move_shed.html')
+
 
 def maintenance_shed(request):
     return render(request, 'canoe_club/maintenance_shed.html')
 
-
-def trips(request):
-    return render(request, 'canoe_club/trips.html')
-
-
-def user_login(request):
-    return render(request, 'canoe_club/login.html')
-
-
-def register(request):
-    return render(request, 'canoe_club/register.html')
-
-def socials(request):
-    today = datetime.datetime.today()
-    social_list = Social.objects.order_by("-date")
-    context_dixt = {}
-    context_dixt["socials"] = social_list
-    return render(request, "canoe_club/socials.html", context_dixt)
-
-def trips(request):
-    today = datetime.datetime.today()
-    trip_list = Trip.objects.order_by("-date")
-    context_dixt = {}
-    context_dixt["trips"] = trip_list
-    return render(request, "canoe_club/trips.html", context_dixt)
-
+def maintenance_shed(request):
+    return render(request, 'canoe_club/maintenance_shed.html')
 
 def kit(request, kit_name_slug):
 
@@ -64,6 +49,46 @@ def kit(request, kit_name_slug):
 
     return render(request, "canoe_club/kit.html", context_dict)
 
+def move_kit(request, kit_name_slug):
+    return render(request, "canoe_club/kit/move_kit.html")
+
+def user_login(request):
+    return render(request, 'canoe_club/login.html')
+
+def user_profile(request):
+    return render(request, 'canoe_club/login/profile.html')
+
+def change_password(request):
+    return render(request, 'canoe_club/login/profile/change_password.html')
+
+def edit_profile(request):
+    return render(request, 'canoe_club/login/profile/edit_profile.html')
+
+def register(request):
+    return render(request, 'canoe_club/register.html')
+
+def socials(request):
+    today = datetime.datetime.today()
+    social_list = Social.objects.order_by("-date")
+    context_dixt = {}
+    context_dixt["socials"] = social_list
+    return render(request, "canoe_club/socials.html", context_dixt)
+
+def add_social(request):
+    return render(request, "canoe_club/socials/add_social.html")
+
+def remove_social(request):
+    return render(request, "canoe_club/socials/remove_social.html")
+
+
+def trips(request):
+    today = datetime.datetime.today()
+    trip_list = Trip.objects.order_by("-date")
+    context_dixt = {}
+    context_dixt["trips"] = trip_list
+    return render(request, "canoe_club/trips.html", context_dixt)
+
+
 def trip(request, trip_name_slug):
 
     try:
@@ -75,7 +100,22 @@ def trip(request, trip_name_slug):
 
     return render(request, "canoe_club/trip.html", context_dict)
 
-def about(request):
-    return render(request, "canoe_club/about.html", {})
+def trip_member(request):
+    return render(request, "canoe_club/trips/trip/member.html")
 
-#  a change
+def add_trip_member(request):
+    return render(request, "canoe_club/trips/trip/member/add_member.html")
+
+def remove_trip_member(request):
+    return render(request, "canoe_club/trips/trip/member/remove_member.html")
+
+def add_trip(request):
+    return render(request, "canoe_club/trips/trip/add_trip.html")
+
+def remove_trip(request):
+    return render(request, "canoe_club/trips/trip/remove_trip.html")
+
+
+
+
+
